6781e61a7c3555456d973726daa1325b163d8bc8
jiayuww <jwang2782@wisc.edu>
2024-11-05 22:43:30 -0600

    init readme, demo imgs, model inference code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SpatialEval
+
+This is the codebase for [Is A Picture Worth A Thousand Words? Delving Into Spatial Reasoning for Vision Language Models](https://arxiv.org/abs/2406.14852). 
+
---
+++ models/model_utils.py
@@ -0,0 +1,15 @@
+import requests
+import os
+from PIL import Image
+from io import BytesIO
+import re
+
+def load_image(image_file):
+    if image_file.startswith('http://') or image_file.startswith('https://'):
+        response = requests.get(image_file)
+        image = Image.open(BytesIO(response.content)).convert('RGB')
+    else:
+        image = Image.open(image_file).convert('RGB')
+    return image
+
+
